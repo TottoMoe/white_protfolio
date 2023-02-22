3bf3eb355e87bbd81256d13f1482cbe630c18ecd
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2023-02-21 16:32:43 -0800

    update socialmedia

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -5,13 +5,27 @@ import { FaHackerrank } from "react-icons/fa";
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsGithub />
+      <a href="https://github.com/TottoMoe" target="_blank" rel="noreferrer">
+        <BsGithub />
+      </a>
     </div>
     <div>
-      <BsLinkedin />
+      <a
+        href="https://www.linkedin.com/in/jennyhjdeng/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
     </div>
     <div>
-      <FaHackerrank />
+      <a
+        href="https://www.hackerrank.com/jennydhj"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaHackerrank />
+      </a>
     </div>
   </div>
 );
